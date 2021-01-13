8cf73fdcd12db43dd606dc8d584224804fe9544f
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2021-01-14 00:59:12 +0200

    spring boot JWT

---
+++ src/main/java/com/gavril/jwt/controller/ResourceController.java
@@ -0,0 +1,58 @@
+package com.gavril.jwt.controller;
+
+import com.gavril.jwt.model.JwtRequest;
+import com.gavril.jwt.model.JwtResponse;
+import com.gavril.jwt.service.UserService;
+import com.gavril.jwt.utility.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class ResourceController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    private String token;
+
+    @GetMapping("/")
+    public String home() {
+        String name = jwtUtility.getUsernameFromToken(token);
+        return "Hello " + name;
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+        this.token = token;
+
+        return new JwtResponse(token);
+    }
+}
